0ba84046dbb1cb46ea3f7ca4afeddea19986f1eb
kiaraRobles <kiara.robles@gmail.com>
2015-10-11 03:26:03 -0400

    Inital commit

---
+++ bitcoinPaperWallet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bitcoinPaperWallet
+//
+//  Created by Kiara Robles on 10/11/15.
+//  Copyright © 2015 Kiara Robles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ bitcoinPaperWalletTests/bitcoinPaperWalletTests.m
@@ -0,0 +1,39 @@
+//
+//  bitcoinPaperWalletTests.m
+//  bitcoinPaperWalletTests
+//
+//  Created by Kiara Robles on 10/11/15.
+//  Copyright © 2015 Kiara Robles. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bitcoinPaperWalletTests : XCTestCase
+
+@end
+
+@implementation bitcoinPaperWalletTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
